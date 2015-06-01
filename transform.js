64eac0bad23eac8e94b493726d9e17255a5d4f4c
Mikko Haapoja <me@mikkoh.com>
2015-06-01 14:06:16 -0400

    Swapped perspective to be applied again

--- transform.js
+++ transform.js
@@ -3,10 +3,13 @@ var getTransformMatrix = require('./lib/getTransformMatrix');
 module.exports = function( item, data ) {
   
   var transform = getTransformMatrix(data);
+  var cssValue;
+  var perspective;
 
   if( transform ) {
-    
-    var cssValue = 'matrix3d(' + Array.prototype.join.call( transform, ',' ) + ')';
+
+    perspective = -1 / transform[ 11 ];
+    cssValue = 'perspective(' + perspective + 'px) matrix3d(' + Array.prototype.join.call( transform, ',' ) + ')';
 
     item.style.transform = cssValue;
     item.style.webkitTransform = cssValue;
