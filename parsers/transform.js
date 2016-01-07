245c004dd08565861d7c5653c7da5927be43e265
Mikko Haapoja <me@mikkoh.com>
2016-01-07 12:49:35 -0500

    Fixed an issue where  was returning matrix3d(

--- parsers/transform.js
+++ parsers/transform.js
@@ -10,7 +10,8 @@ module.exports = function(target, state) {
   if( transform ) {
 
     perspective = -1 / transform[ 11 ];
-    cssValue = 'perspective(' + perspective + 'px) matrix3d(' + stringify(transform) + ')';
+    cssValue = 'perspective(' + perspective + 'px) ' + stringify(transform);
+    console.log(stringify(transform));
 
     target.style.transform = cssValue;
     target.style.webkitTransform = cssValue;
