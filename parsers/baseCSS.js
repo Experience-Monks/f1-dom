7b03c4fdd1b0996b57bc39b7b7aa85952c747060
marcod1419 <marcod1419@gmail.com>
2016-07-28 16:29:21 -0400

    Added simple try-catch handling for CSS errors

--- parsers/baseCSS.js
+++ parsers/baseCSS.js
@@ -1,7 +1,12 @@
 var style = require('dom-css');
 
 module.exports = function(target, state) {
-  if(state.style) {
-    style(target, state.style);  
+  try{
+    if(state.style) {
+      style(target, state.style);  
+    }
+  }
+  catch(err){
+    console.error("Missing a style object in a state belonging to", target.getAttribute('data-f1'))
   }
 };
--- parsers/transformOrigin.js
+++ parsers/transformOrigin.js
@@ -1,19 +1,24 @@
 module.exports = function(target, state) {
 
-  if(state.style && Array.isArray(state.style.transformOrigin)) {
-    
-    var anchor = state.style.transformOrigin;
-    var cssValue = anchor
-    .map(function(value) {
-      return value * 100 + '%'
-    })
-    .join(' ');
+  try{
+    if(state.style && Array.isArray(state.style.transformOrigin)) {
+      
+      var anchor = state.style.transformOrigin;
+      var cssValue = anchor
+      .map(function(value) {
+        return value * 100 + '%'
+      })
+      .join(' ');
 
 
-    target.style.transformOrigin = cssValue;
-    target.style.webkitTransformOrigin = cssValue;
-    target.style.msTransformOrigin = cssValue;
-    target.style.MozTransformOrigin = cssValue;
-    target.style.OTransformOrigin = cssValue;
+      target.style.transformOrigin = cssValue;
+      target.style.webkitTransformOrigin = cssValue;
+      target.style.msTransformOrigin = cssValue;
+      target.style.MozTransformOrigin = cssValue;
+      target.style.OTransformOrigin = cssValue;
+    }
+  }
+  catch(err){
+    console.log("Transform Error")
   }
 };
