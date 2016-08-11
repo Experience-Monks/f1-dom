e6e8959c37753fc06ff26e4bd4bea0d7a7a16084
marcod1419 <marcod1419@gmail.com>
2016-08-11 17:00:38 -0400

    Added check for invalid transform values

--- parsers/transform.js
+++ parsers/transform.js
@@ -1,7 +1,21 @@
 var getTransformMatrix = require('../lib/getTransformMatrix');
 
 module.exports = function(target, state) {
-  
+
+  var targetName = target.getAttribute('data-f1');
+
+  if(state.style.translate){
+    validateTransform(state.style.translate, "translate", targetName);
+  }
+
+  if(state.style.rotate){
+    validateTransform(state.style.rotate, "rotate", targetName);
+  }
+
+  if(state.style.scale){
+    validateTransform(state.style.scale, "scale", targetName);
+  }
+
   var cssValue = getTransformMatrix(state);
 
   if( cssValue ) {
@@ -11,4 +25,18 @@ module.exports = function(target, state) {
     target.style.mozTransform = cssValue;
     target.style.ieTransform = cssValue;
   }
+};
+
+function validateTransform(transform, transformType, targetName){
+
+  let validation = 0;
+
+  for (var i = 0; i < transform.length; i++){
+    validation += transform[i]
+    if (isNaN(validation)){
+      console.warn("Invalid value in a " + transformType + " definition for '" + targetName + "'.");
+      break;
+    }
+  }
+
 };
